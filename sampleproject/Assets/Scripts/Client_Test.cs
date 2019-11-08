f0cc121ef68111cccc1e447a04532932e40a8eb7
Richard Hoagland <richiehoagland@gmail.com>
2019-11-08 11:59:55 -0500

    initial commit

---
+++ sampleproject/Assets/Scripts/Client_Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+namespace Network
+{
+    public class Client_Test : MonoBehaviour
+    {
+        public MulticastDiscovery Multicast;
+
+        private void Awake()
+        {
+            Multicast.OnReceive.AddListener(MyAction);
+        }
+
+        private void MyAction(IPAddress arg0)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
